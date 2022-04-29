524e2b8f02cafb6f325da484c35dc01f3b34327b
AaEzha <aaezha@gmail.com>
2022-04-29 11:24:34 +0700

    fix: home auth

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', DashboardController::class)->name('dashboard');
+Route::get('/home', DashboardController::class)->name('dashboard')->middleware('auth');
